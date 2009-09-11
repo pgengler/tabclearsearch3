ce9357cf1036db3269227ca64579cb5411e7dcd5
Phil Gengler <phil@pgengler.net>
2009-09-11 15:29:49 +0000

    Move 'content' directory out of 'chrome' directory; fix error with clearSearchBar function (Todo #1740)
    git-svn-id: svn+ssh://svn.pgengler.net/srv/svn/tabclearsearch3@3 09b4d297-6744-4c8f-84d3-02ed3fc2305a

--- content/tabclearsearch.js
+++ content/tabclearsearch.js
@@ -1,20 +1,20 @@
 var tabclearsearch =
 {
-	init: function()
-	{
-		// Get tab container
-		var container = gBrowser.tabContainer;
-	
-		// Add listener for when a new tab is opened
-		container.addEventListener("TabOpen", clearSearchBar, false);
-	},
-
 	clearSearchBar: function()
 	{
 		var bar = document.getElementById("searchbar");
 		var searchBar = document.getAnonymousElementByAttribute( bar, "type", "autocomplete" );
 		searchBar.value = '';
 	},
+
+	init: function()
+	{
+		// Get tab container
+		var container = gBrowser.tabContainer;
+	
+		// Add listener for when a new tab is opened
+		container.addEventListener("TabOpen", tabclearsearch.clearSearchBar, false);
+	}
 };
 
 
